7528baa844f9c7d0ee46fa422542d12aaecc97c5
VWDREAM <wanjiruvirginiah26@gmail.com>
2025-04-14 17:17:58 +0300

    expenseitem adjustments

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 
 function ExpenseItem({ expense }) {
   return (
-    <tr className="hover:bg-gray-50">
-      <td className="px-4 py-2">{expense.description}</td>
-      <td className="px-4 py-2">${expense.amount.toFixed(2)}</td>
-      <td className="px-4 py-2">{expense.category}</td>
+  
     </tr>
   );
 }
